02628d7d4f628564a9ed80d7903653add0ebf5dd
Caldheron <camille.guilbaud@protonmail.ch>
2021-02-04 20:31:42 +0100

    C le début d'un monde plein de possibilités!

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HelloooWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> strlist = new List<string>();
+            strlist.Add("test");
+            strlist.Add("of");
+            strlist.Add("Netcore");
+
+            foreach(var str in strlist)
+            {
+                Console.WriteLine(str);
+            }
+            
+        }
+    }
+}
